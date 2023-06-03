ec5666a25baeb52495deffe08e2b0524bc9b6124
drisyaathul <drisyak809@gmail.com>
2023-06-04 01:28:19 +0800

    Indian States Census Analyser Problem

---
+++ src/main/java/com/indianstatecensusanalyser/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.indianstatecensusanalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("**** Welcome to Indian States Census Analyser Problem ****");
+    }
+}
